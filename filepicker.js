8c4f12a00ab9387524daf6a293b2c281696d90b1
Matheus <matheusmcavalcante94@gmail.com>
2017-05-25 23:39:06 -0300

    Funcionalidade drag n' drop

---
+++ filepicker.js
@@ -0,0 +1,39 @@
+var imageLoader = document.getElementById('filePhoto');
+    imageLoader.addEventListener('change', handleImage, false);
+
+function handleImage(e) {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        $('#uploader img').attr('src',event.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+    
+}
+
+var dropbox;
+dropbox = document.getElementById("uploader");
+dropbox.addEventListener("dragenter", dragenter, false);
+dropbox.addEventListener("dragover", dragover, false);
+dropbox.addEventListener("drop", drop, false);
+
+function dragenter(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function dragover(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function drop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  //Para ver as propriedades de (e) digite
+  //console.log(e);
+  var dt = e.dataTransfer;
+  var files = dt.files;
+  
+  //Esta linha de código dispara a função 'handleImage' (evento de mudança imageLoader)
+  imageLoader.files = files;
+}
